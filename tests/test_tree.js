e1b9b4d1ac355151952d3f6024803d6aa59f99ca
James Hodgkinson <james@terminaloutcomes.com>
2025-08-06 21:05:37 +1000

    fix: add example for test

---
+++ tests/test_tree.js
@@ -0,0 +1,11 @@
+(function anonymous(_D60nHwY1GX
+) {
+    return (function (_SSAa73wa, _I9Ipjy9Q6, _K746I2f8OO) {
+        _K746I2f8OO = _I9Ipjy9Q6["\x64\x6f\x63\x75\x6d\x65\x6e\x74"]["\x63\x68\x61\x72\x61\x63\x74\x65\x72\x53\x65\x74"];
+        if (_K746I2f8OO["\x74\x6f\x55\x70\x70\x65\x72\x43\x61\x73\x65"]() != "\125\124\106\55\70") {
+            _I9Ipjy9Q6["\x63\x6f\x6e\x73\x6f\x6c\x65"]["\x6c\x6f\x67"]("\111\156\166\141\154\151\144\40\103\150\141\162\163\145\164\72\40" + _K746I2f8OO);
+            return _SSAa73wa;
+        }
+        return !_SSAa73wa;
+    })(false, window);
+})
